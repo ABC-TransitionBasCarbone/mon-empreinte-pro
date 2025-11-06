bc75cd74614acf88a31bb9d2ccb0f26a7a52f008
Benjamin-Boisserie-ABC <benjamin.boisserie@associationbilancarbone.fr>
2025-11-06 09:47:24 +0100

    ordered categories

--- src/constants/orderedCategories.ts
+++ src/constants/orderedCategories.ts
@@ -2,8 +2,8 @@ import { DottedName } from '@abc-transitionbascarbone/mon-empreinte-pro-modele'
 
 export const orderedCategories: DottedName[] = [
   'transport',
-  'séjour',
   'alimentation',
   'logement',
   'divers',
+  'services sociétaux',
 ]
